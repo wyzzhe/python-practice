4f2cf3d7bf294a186d57a13dd530ad620bb93210
wyzzhe <a545567422@gmail.com>
2025-10-30 19:29:29 +0800

    fix: brand list

--- vr_pic_to_prod/brand_list.py
+++ vr_pic_to_prod/brand_list.py
@@ -1,29 +1,28 @@
 brand_list = [
     # 美妆护肤类
-    # "HERMES",
-    # "LANCOME",
+    "HERMES", "LANCOME",
     # # 时尚服饰类
     "PRADA", "MICHAELKORS",
-    # # 运动休闲类
-    # "安踏", "耐克",
-    # # 珠宝首饰类
-    # "HEFANG", "六福珠宝",
-    # # 家居用品类
-    # "睿锦尚品", "宜家",
-    # # 餐饮类
-    # "O'eat", "星巴克",
-    # # 儿童用品类
-    # "阿吉豆", "巴拉巴拉",
-    # # 自行车类
-    # "BROMPTON", "Specialized",
-    # # 箱包类
-    # "MCM", "TUMI",
-    # # 鞋履类
-    # "BIRKENSTOCK", "HUGO BOSS",
-    # # 汽车类
-    # "蔚来", "特斯拉",
-    # # 户外运动类
-    # "PeakPerformance", "K-Way",
+    # 运动休闲类
+    "安踏", "耐克",
+    # 珠宝首饰类
+    "HEFANG", "六福珠宝",
+    # 家居用品类
+    "睿锦尚品", "宜家",
+    # 餐饮类
+    "O'eat", "星巴克",
+    # 儿童用品类
+    "阿吉豆", "巴拉巴拉",
+    # 自行车类
+    "BROMPTON", "Specialized",
+    # 箱包类
+    "MCM", "TUMI",
+    # 鞋履类
+    "BIRKENSTOCK", "HUGO BOSS",
+    # 汽车类
+    "蔚来", "特斯拉",
+    # 户外运动类
+    "PeakPerformance", "K-Way",
 ]
 
 vr_loc_list = [6]
